f96049ac38ed2b6e628b8a45e25cfc6d375d5875
siavash sepahi <sepahisiavash@gmail.com>
2022-01-29 16:29:45 +0330

    add demand

--- main.py
+++ main.py
@@ -24,19 +24,26 @@ def Read_PM2xxx(rs_485_address: int, device_type: int) -> dict[str, Union[int, f
 
     incoming_data_part1 = client.multiple_register_read("holding", 2699, 12, "FLOAT32")
     num = incoming_data_part1[0]
+
     if not num != num:
         # Todo:bar asas device type nis
         incoming_data_part2 = client.multiple_register_read("holding", 2999, 6, "FLOAT32")
         incoming_data_part3 = client.multiple_register_read("holding", 3019, 9, "FLOAT32")
         incoming_data_part4 = client.multiple_register_read("holding", 3053, 20, "FLOAT32")
         incoming_data_part5 = client.multiple_register_read("holding", 3109, 1, "FLOAT32")
-        incoming_data_part6 = client.multiple_register_read("holding", 3763, 1, "FLOAT32")
-        incoming_data_part7 = client.multiple_register_read("holding", 3779, 1, "FLOAT32")
-        incoming_data_part8 = client.multiple_register_read("holding", 3795, 1, "FLOAT32")
+        incoming_data_part6 = client.multiple_register_read("holding", 3763, 4, "FLOAT32")
+        incoming_data_part6_1 = client.multiple_register_read("holding", 3771, 1, "DATETIME")
+        incoming_data_part7 = client.multiple_register_read("holding", 3779, 4, "FLOAT32")
+        incoming_data_part7_1 = client.multiple_register_read("holding", 3787, 1, "DATETIME")
+        incoming_data_part8 = client.multiple_register_read("holding", 3795, 4, "FLOAT32")
+        incoming_data_part8_1 = client.multiple_register_read("holding", 3803, 1, "DATETIME")
         incoming_data_part9 = client.multiple_register_read("holding", 3875, 4, "FLOAT32")
         incoming_data_part10 = client.multiple_register_read("holding", 3883, 1, "DATETIME")
 
         try:
+            incoming_data_part6 = incoming_data_part6 + incoming_data_part6_1
+            incoming_data_part7 = incoming_data_part7 + incoming_data_part7_1
+            incoming_data_part8 = incoming_data_part8 + incoming_data_part8_1
             incoming_data = incoming_data_part1 + \
                             incoming_data_part2 + \
                             incoming_data_part3 + \
@@ -110,16 +117,28 @@ def Read_PM2xxx(rs_485_address: int, device_type: int) -> dict[str, Union[int, f
                 "Frequency": incoming_data[47],
 
                 "Active_Power_Last_Demand": incoming_data[48],
+                "Active_Power_Present_Demand": incoming_data[49],
+                "Active_Power_Predicted_Demand": incoming_data[50],
+                "Active_Power_Peak_Demand": incoming_data[51],
+                "Active_Power_PK_DT_Demand": incoming_data[52],
 
-                "Reactive_Power_Last_Demand": incoming_data[49],
+                "Reactive_Power_Last_Demand": incoming_data[53],
+                "Reactive_Power_Present_Demand": incoming_data[54],
+                "Reactive_Power_Predicted_Demand": incoming_data[55],
+                "Reactive_Power_Peak_Demand": incoming_data[56],
+                "Reactive_Power_PK_DT_Demand": incoming_data[57],
 
-                "Apparent_Power_Last_Demand": incoming_data[50],
+                "Apparent_Power_Last_Demand": incoming_data[58],
+                "Apparent_Power_Present_Demand": incoming_data[59],
+                "Apparent_Power_Predicted_Demand": incoming_data[60],
+                "Apparent_Power_Peak_Demand": incoming_data[61],
+                "Apparent_Power_PK_DT_Demand": incoming_data[62],
 
-                "Current_Avg_Last_Demand": incoming_data[51],
-                "Current_Avg_Present_Demand": incoming_data[52],
-                "Current_Avg_Predicted_Demand": incoming_data[53],
-                "Current_Avg_Peak_Demand": incoming_data[54],
-                "Current_Avg_PK_DT_Demand": incoming_data[55],
+                "Current_Avg_Last_Demand": incoming_data[63],
+                "Current_Avg_Present_Demand": incoming_data[64],
+                "Current_Avg_Predicted_Demand": incoming_data[65],
+                "Current_Avg_Peak_Demand": incoming_data[66],
+                "Current_Avg_PK_DT_Demand": incoming_data[67],
             }
 
             # json_data_out = json.dumps(dict_data_out, indent=2)
@@ -133,24 +152,25 @@ def Read_PM2xxx(rs_485_address: int, device_type: int) -> dict[str, Union[int, f
     else:
         return {"substation_id": -1}
 
+
 rs_485_address = 1
 # q = Read_PM2xxx(2, 1)
 client.unit_id(rs_485_address)
 incoming_data_part1 = client.multiple_register_read("holding", 21299, 10, "FLOAT32")
 incoming_data_part2 = client.multiple_register_read("holding", 21321, 16, "FLOAT32")
-incoming_data = incoming_data_part1+incoming_data_part2
+incoming_data = incoming_data_part1 + incoming_data_part2
 
 q = {
-                "substation_id": electrical_substation_id,
-                "unitId": rs_485_address,
+    "substation_id": electrical_substation_id,
+    "unitId": rs_485_address,
 
-                "THD_Current_A": incoming_data[0],
-                "THD_Current_B": incoming_data[1],
-                "THD_Current_C": incoming_data[2],
-                "THD_Voltage_A_N": incoming_data[3],
-                "THD_Voltage_B_N": incoming_data[4],
-                "THD_Voltage_C_N": incoming_data[5]
-    }
+    "THD_Current_A": incoming_data[0],
+    "THD_Current_B": incoming_data[1],
+    "THD_Current_C": incoming_data[2],
+    "THD_Voltage_A_N": incoming_data[3],
+    "THD_Voltage_B_N": incoming_data[4],
+    "THD_Voltage_C_N": incoming_data[5]
+}
 
 for key, value in q.items():
     print(key, ' : ', value)
